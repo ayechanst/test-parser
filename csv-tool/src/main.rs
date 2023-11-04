fd8b889e161df9b6515c43da392b64bead627cb3
Chad Bro <ayechan.sant@gmail.com>
2023-11-04 14:28:20 -0700

    main

--- csv-tool/src/main.rs
+++ csv-tool/src/main.rs
@@ -1,3 +1,14 @@
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "csv.pest"]
+pub struct CSVParser;
+
 fn main() {
-    println!("Hello, world!");
+    let successful_parse = CSVParser::parse(Rule::field, "-173.15");
+    println!("{:?}", successful_parse);
+
+    let unsuccessful_parse = CSVParser::parse(Rule::field, "this is not a number");
+    println!("{:?}", unsuccessful_parse);
 }
